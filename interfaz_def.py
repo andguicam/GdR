eb70ce58045de097acae86f4f9de151b7450a855
andguicam <andguicam@alum.us.es>
2021-05-04 19:23:37 +0200

    integrado pero falta pasarle oid

--- interfaz_def.py
+++ interfaz_def.py
@@ -3,9 +3,15 @@ import sys
 from funciones.get_handler import *
 from easysnmp import Session
 from funciones.cargar_mibs import *
+from funciones.get_handler import get_handler
+from funciones.set_handler import set_handler
+from funciones.get_next_handler import get_next_handler
+
+
 #funcion para iniciar session
 #creamos una session de snmp
 session = Session(hostname='localhost', community='public', version=2)
+
 
 #creamos la pestaña principal
 app = Tk() 
@@ -24,7 +30,7 @@ def obtener():
 		system = system_mib()
 		#cargamos los valores
 		sys_value = StringVar(app)
-		sys_value.set("Selecciona un objecto")
+		sys_value.set("Selecciona un objeto")
 		sys_menu = OptionMenu (app, sys_value, *system).pack()
 		#valor que se pasa a las funciones de los botones
 		parametro = sys_value.get()
@@ -33,14 +39,19 @@ def obtener():
 	#añadimos los botones de get, set y getnext
 	bottomframe = Frame(app)
 	bottomframe.pack(side=BOTTOM)
-	button_set= Button(app, text="Set", command = lambda:set_fun(parametro))
-	button_get=Button(app, text="Get", command = lambda:get_handler(session, parametro))
-	button_getNext=Button(app, text="Get next", command = lambda:getnext_fun(parametro))
-	button_set.place(x=200, y=550)
-	button_get.place(x=250, y =550)
-	button_getNext.place(x=300, y =550)
+	button_set= Button(app, text="Set", command = lambda:set_handler(session,parametro,campo_respuesta))
+	button_get=Button(app, text="Get", command = lambda:get_handler(session, parametro,campo_respuesta))
+	button_getNext=Button(app, text="Get next", command = lambda:get_next_handler(session,parametro,campo_respuesta))
+	button_set.place(x=200, y=450)
+	button_get.place(x=250, y =450)
+	button_getNext.place(x=300, y =450)
 
+	campo_respuesta = Label(app, text="Respuesta de la operación:")
+	campo_respuesta.place(x=0, y=500)
 	
 Button (app, text="Obtener", command=obtener).pack()
+
+
+
 
 app.mainloop()
