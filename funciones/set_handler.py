84c8e5397cbb4ee6a6825fe85d9c7597e825aa58
andguicam <andguicam@alum.us.es>
2021-05-04 15:40:38 +0200

    funciones para el manejo de get. set y getnext

--- funciones/set_handler.py
+++ funciones/set_handler.py
@@ -1,16 +1,22 @@
 import easysnmp
+from pysnmp import hlapi
 
+from funciones.quicksnmp import set
 #Recibe como parámetro la sesión y el OID (y la ventana), y actualiza la ventana con la respuesta
 
-def get_handler(sesion,oid,etiquetaRespuesta):
+def set_handler(sesion,oid,valor,etiquetaRespuesta):
 
     #Comprobamos que se ha pasado un OID
     if oid:
         try:
-            #Realizo peeticion y obtengo los valores
-            get_response = sesion.get(oid)
-            oid=get_response.oid
-            valor=get_response.value
+            #Realizo peticion y obtengo los valores
+            ip=sesion.hostname
+            comunidad=sesion.community
+            set_response= set(ip, {str(oid): str(valor)},
+                               hlapi.CommunityData(comunidad))
+            datos = list(set_response.items())
+            oid = datos[0][0]
+            valor = datos[0][1]
             #Actualizamos la etiqueta referente al campo de respuestas para mostrar el resultado de la operacion
             etiquetaRespuesta.config(text="Respuesta de {0}: '{1}'".format(oid,valor),bg="SpringGreen2")
         except:
