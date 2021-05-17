f262e749bec19c4712357858fd97cbf812faa020
andguicam <andguicam@alum.us.es>
2021-05-17 15:21:31 +0200

    script de instalacion de dependencias

---
+++ instalar_dependencias.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+sudo apt install libsnmp-dev snmp-mibs-downloader gcc python-dev  snmp python3 python3-pip python3-tk -y
+pip3 install puresnmp easysnmp pysnmp
+echo "---------------------------------------------------"
+echo "INSTALACION DE DEPENDENCIAS COMPLETADA."
+echo "Por favor ejecute el fichero interfaz_inicio.py para ejecutar el programa."
+echo "Ejemplo de ejecucion: $ python3 interfaz_inicio.py"
+echo "---------------------------------------------------"
