eb6d6102e66956cde9608e85b187dc96a8947eb6
andguicam <79918281+andguicam@users.noreply.github.com>
2021-05-19 21:04:04 +0200

    snmptable correcto

--- funciones/snmptable.py
+++ funciones/snmptable.py
@@ -1,4 +1,9 @@
+from os import error
+from funciones.comprobar_version import comprobar_version
 import subprocess
+from funciones.comprobar_version import comprobar_version
+from funciones.comprobar_error import comprobar_error
+
 
 def snmptable(ip,comunidad,oid):
     linea=3 #Linea donde estan los indices de la tabla
@@ -6,17 +11,34 @@ def snmptable(ip,comunidad,oid):
     l_limpia=[] #Contiene en la primera posicion la linea con los titulos de las columnas
                 #Es lo que queremos devolver
 
+    #Necesitamos comprobar la version porque en MacOS se proporciona la version 5.9 de snmptable,
+    #que tiene una sintaxis distinta a la version 5.8 que es la que se proporciona en Linux (Ubuntu 20.04)
 
-    out = subprocess.Popen(['snmptable','-v','1','-c', comunidad, ip, oid],
-                        stdout=subprocess.PIPE,
-                        stderr=subprocess.STDOUT)
+    version = subprocess.Popen(['snmptable','-V'],
+                               stdout=subprocess.PIPE,
+                               stderr=subprocess.STDOUT)
+    
+    copia=version.communicate()
+    if comprobar_version() == '5.9':
+        out = subprocess.Popen(['snmptable','-v', '1', '-c', comunidad, ip, oid],
+                               stdout=subprocess.PIPE,
+                               stderr=subprocess.STDOUT)
+        
+    else:
+        out = subprocess.Popen(['snmptable',ip,oid,'-c',comunidad,'-v','1','-O','a','-m','ALL'],
+                            stdout=subprocess.PIPE,
+                            stderr=subprocess.STDOUT)
+
 
     stdout,stderr=out.communicate()
     lineas=stdout.splitlines()
     for el in lineas:
         l.append(el.decode())
     try:
-        res=" ".join(l[2].split())
+        if comprobar_error(stdout):
+            res=" ".join(l[3].split())
+        else:
+            res = " ".join(l[2].split())
     except:
         #Si tenemos una excepcion es porque no existe tabla para el OID introducido
         return "OID invalido. No hay tabla asociada"
