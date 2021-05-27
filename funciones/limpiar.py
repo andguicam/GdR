d9b6c80116d1e764ccbb698a4b51ba74145e0409
Reyossrom99 <82873755+Reyossrom99@users.noreply.github.com>
2021-05-27 20:55:56 +0200

    Add files via upload

--- funciones/limpiar.py
+++ funciones/limpiar.py
@@ -1,10 +1,8 @@
 from tkinter import *
 def limpiar(oid_entry, campo_respuesta): 
 	oid_entry.delete(0,'end')
-	for widgets in campo_respuesta.winfo_children():
-		widgets.destroy()
+	campo_respuesta.config(text=" ")
 def limpiar_campo(campo_respuesta):
-	for widgets in campo_respuesta.winfo_children():
-		widgets.destroy()
+	campo_respuesta.config(text=" ")
 def limpiar_frame(respuesta_frame):
 	respuesta_frame.destroy()
